773e77dd1364ca8badc06a8b1901e1e9803eb6e8
Manuel Garcia <manuel.gar@samsung.com>
2015-05-29 18:15:10 -0700

    Final Soln for haunted house, not clean.

--- week15Challenge/hauntedhouse.c
+++ week15Challenge/hauntedhouse.c
@@ -86,16 +86,47 @@ void printPeopleTable(unsigned long n){
 void sortByL(unsigned long *start_n,unsigned long n){
     unsigned long i,j;
     unsigned long long tempH,tempL,tempGap;
-    unsigned long long valueH = people[*start_n][0];
+    unsigned long long valueGapIni = people[*start_n][0];
     //printf("at sort by l start_n = %lu\n",*start_n);
     for(i = *start_n; i < n ;i++){
         for(j = *start_n ; j < n ;j++){
             //printf("at sortByL\n");
-            if(people[j][2]!= valueH){
+            if(people[j][2] != valueGapIni){
+                printf("valulegapini =%llu current =%llu \n",valueGapIni,people[j][2]);
                 *start_n =i;
                 break;
             }
             if(people[j][0] < people[j+1][0]){
+                tempL = people[j+1][0];
+                tempH = people[j+1][1];
+                tempGap = people[j+1][2];
+
+                people[j+1][0] = people[j][0];
+                people[j+1][1] = people[j][1];
+                people[j+1][2] = people[j][2];
+
+                people[j][0] = tempL;
+                people[j][1] = tempH;
+                people[j][2] = tempGap;
+            }
+        }
+    }
+    *start_n=i;
+}
+
+void sortByH(unsigned long *start_n,unsigned long n){
+    unsigned long i,j;
+    unsigned long long tempH,tempL,tempGap;
+    unsigned long long valueH = people[*start_n][1];
+    //printf("at sort by l start_n = %lu\n",*start_n);
+    for(i = *start_n; i < n ;i++){
+        for(j = *start_n ; j < n ;j++){
+            //printf("at sortByL\n");
+            if(people[j][2] != valueH){
+                *start_n =i;
+                break;
+            }
+            if(people[j][1] < people[j+1][1]){
                 tempL = people[j+1][0];
                 tempH = people[j+1][1];
                 tempGap = people[j+1][2];
@@ -131,18 +162,20 @@ unsigned long long getMaximumOutput(unsigned long n){
         if(people[i][1]  < minH)
             minH = people[i][1];
 
-        if(people[i][1] <= max ){ //checking H
+        if(people[i][1] <= max &&
+           people[i][2] != 0){
             i=0;
+            //printf("decreasing max H =%llu max=%llu \n",people[i][1] ,max);
             max--;
-            maxL=0;
+            //maxL=0;
             continue;
         }
         i++;
     }
     i=0;
-    //printf("maxL = %llu max total=%llu \n",maxL,max+1);
-    if(maxL >= max+1)
-        return 0;
+    //printf("maxL = %llu max total=%llu \n",maxL,max);
+    //if(maxL >= max+1)
+    //    return 0;
     while( i < max ){
         if(people[i][0]  > maxL)
             maxL = people[i][0];
@@ -153,14 +186,44 @@ unsigned long long getMaximumOutput(unsigned long n){
         if(people[i][1]  < minH)
             minH = people[i][1];
 
-        if( people[i][0] > max )
+        if( people[i][0] > max ){//L
             max--;
+        }
         i++;
     }
 
     return max;
 }
+unsigned long long getMaximumOutput2(unsigned long n)
+{
+    unsigned long long max = maxH+1;
 
+    unsigned long long countPeople=0;
+    unsigned int i =0;
+
+restart :
+    countPeople=0;
+    i=0;
+    while(i < n){
+        /*printf("before check L = %llu H= %llu max -1 = %llu \n",
+          people[i][0] , people[i][1] ,max - 1 );*/
+        if(people[i][0] <= max - 1 && 
+           people[i][1] >= max - 1){
+            //printf("%u matches \n",i);
+            countPeople++;
+            if(countPeople == max)
+                break;
+        }
+        i++;
+    }
+    //printf("before compare max = %llu countPeople = %llu \n",max,countPeople);
+    if(max != countPeople){
+        //printf("restarting max = %llu countPeople -%llu\n",max,countPeople);
+        max--;
+        goto restart;
+    }
+    return max-1;
+}
 int main() {
     unsigned long start_n=0,n,l,h;
     unsigned long long max =0;
@@ -169,12 +232,19 @@ int main() {
     scanf("%lu",&n);
 
     fillPeopleChoices(n);
-    sortByGap(n);
+    /*sortByGap(n);
     while(start_n<n){
         sortByL(&start_n,n);
     }
-    //printPeopleTable(n);
-    max = getMaximumOutput(n);
+    printPeopleTable(n);
+    start_n=0;
+    while(start_n<n){
+        sortByH(&start_n,n);
+    }
+    printf("=================\n");
+    printPeopleTable(n);
+    */
+    max = getMaximumOutput2(n);
     if(max>0)
         printf("%llu\n",max+1);
     else
